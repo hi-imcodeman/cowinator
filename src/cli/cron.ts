e7b183ca73126eb0cb389e35fff092694fd246b2
Asraf Ali <asraf.cse@gmail.com>
2021-05-23 08:19:04 +0530

    chore: cron modified

--- src/cli/cron.ts
+++ src/cli/cron.ts
@@ -2,59 +2,59 @@ import cron from 'node-cron'
 import { getStats } from './api'
 import moment from 'moment'
 
-cron.schedule('*/15 * * * *', () => {
-    [0,1,2,3].forEach(i=>{
-        getStats({
-            state:'tamil nadu',
-            district: 'chennai',
-            tgChannel: '@cowinator_tamilnadu',
-            d: moment().add(i,'days').format('MM-DD-YYYY')
-        })
+// cron.schedule('*/15 * * * *', () => {
+//     [0,1,2,3].forEach(i=>{
+//         getStats({
+//             state:'tamil nadu',
+//             district: 'chennai',
+//             tgChannel: '@cowinator_tamilnadu',
+//             d: moment().add(i,'days').format('MM-DD-YYYY')
+//         })
+//     })
+// });
+
+cron.schedule('5,20,35,50 * * * *', () => {
+    getStats({
+        state:'tamil nadu',
+        district: 'nagapattinam',
+        tgChannel: '@cowinator_tamilnadu'
     })
 });
 
-// cron.schedule('5,20,35,50 * * * *', () => {
-//     getStats({
-//         state:'tamil nadu',
-//         district: 'nagapattinam',
-//         tgChannel: '@cowinator_tamilnadu'
-//     })
-// });
+cron.schedule('10,25,40,55 * * * *', () => {
+    getStats({
+        state:'tamil nadu',
+        district: 'chennai',
+        tgChannel: '@cowinator_tamilnadu'
+    })
+});
 
-// cron.schedule('10,25,40,55 * * * *', () => {
-//     getStats({
-//         state:'tamil nadu',
-//         district: 'chennai',
-//         tgChannel: '@cowinator_tamilnadu'
-//     })
-// });
+cron.schedule('11,26,41,56 * * * *', () => {
+    getStats({
+        state:'tamil nadu',
+        district: 'Kanchipuram',
+        tgChannel: '@cowinator_tamilnadu'
+    })
+});
 
-// cron.schedule('11,26,41,56 * * * *', () => {
-//     getStats({
-//         state:'tamil nadu',
-//         district: 'Kanchipuram',
-//         tgChannel: '@cowinator_tamilnadu'
-//     })
-// });
+cron.schedule('0,30 * * * *', () => {
+    getStats({
+        state:'tamil nadu',
+        tgChannel: '@cowinator_tamilnadu'
+    })
+});
 
-// cron.schedule('0,30 * * * *', () => {
-//     getStats({
-//         state:'tamil nadu',
-//         tgChannel: '@cowinator_tamilnadu'
-//     })
-// });
+cron.schedule('12,27,42,57 * * * *', () => {
+    getStats({
+        state:'karnataka',
+        district: 'bbmp',
+        tgChannel: '@cowinator_karnataka'
+    })
+});
 
-// cron.schedule('12,27,42,57 * * * *', () => {
-//     getStats({
-//         state:'karnataka',
-//         district: 'bbmp',
-//         tgChannel: '@cowinator_karnataka'
-//     })
-// });
-
-// cron.schedule('5,35 * * * *', () => {
-//     getStats({
-//         state:'karnataka',
-//         tgChannel: '@cowinator_karnataka'
-//     })
-// });
+cron.schedule('5,35 * * * *', () => {
+    getStats({
+        state:'karnataka',
+        tgChannel: '@cowinator_karnataka'
+    })
+});
